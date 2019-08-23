9208c47f97c7846ea7d93c65d28d22c2083ef84b
4m4r199707 <4m4rarts@gmail.com>
2019-08-24 01:23:16 +0530

    establishing middleware for secure routing

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,8 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
-
 const User = require('../models//User');
+
 // @route POST api/users
 //@desc    Regitster a user
 //@access Public
