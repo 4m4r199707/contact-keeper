eeecf902ef216bf67e3dc3cd8e20097134d903d3
4m4r199707 <4m4rarts@gmail.com>
2019-08-30 17:03:58 +0530

    Updation and Delete functionality

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,7 +1,7 @@
 import React, {useContext} from 'react'
 import PropTypes from 'prop-types';
 import ContactContext from '../../context/contact/contactContext';
-import { DELETE_CONTACT } from '../../context/types';
+
 
 
 const ContactItem = ({contact}) => {
@@ -9,19 +9,21 @@ const ContactItem = ({contact}) => {
     const contactContext = useContext(ContactContext);
     const { deleteContact, setCurrent, clearCurrent} = contactContext;
 
-    const { id , name, email, phone, type } = contact;
+    const { _id , name, email, phone, type } = contact;
 
     const onDelete = ()=>{
-        deleteContact(id);
+        deleteContact(_id);
         clearCurrent();
     };
     return (
         <div className='card bg-light'>
             <h3 className="text-primary text-left">
-                {name}{' '}<span style={{float: 'right'}} className={'badge ' + 
+                {name}{' '}<span style={{float: 'right'}} 
+                className={'badge ' + 
                 (type === 'professional' ? 
                 'badge-success' : 'badge-primary')}>
-                {type.charAt(0).toUpperCase() + type.slice(1)}</span>
+                {type.charAt(0).toUpperCase() + type.slice(1)}
+                </span>
             </h3>
             <ul className="list">
             {email && (
@@ -37,8 +39,10 @@ const ContactItem = ({contact}) => {
             )}
             </ul>  
             <p>
-                <button className='btn btn-dark btn-sm' onClick={()=>setCurrent(contact)}>Edit</button>
-                <button className='btn btn-danger btn-sm' onClick={onDelete}>Delete</button>
+                <button className='btn btn-dark btn-sm' 
+                onClick={()=>setCurrent(contact)}>Edit</button>
+                <button className='btn btn-danger btn-sm' 
+                onClick={onDelete}>Delete</button>
             </p>          
         </div>
     )
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -62,9 +62,37 @@ const ContactState = props => {
 
 
     // Delete contact
-    const deleteContact = id => {
-        dispatch({ type: DELETE_CONTACT, payload: id});
+    const deleteContact = async id => {
+        try {
+            const res = await axios.delete
+            (`/api/contacts/${id}`);
+            dispatch({ type: DELETE_CONTACT, payload: id});
+        } catch (error) {
+            dispatch({ type: CONTACT_ERROR,
+            payload : error.response.msg });
+        }
     };
+
+
+        // Update Contact
+    const updateContact = async contact => {
+        const config ={
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        }
+
+        try {
+            const res = await axios.put
+            (`/api/contacts/${contact._id}`, contact, config);
+
+            dispatch({ type: UPDATE_CONTACT, payload: res.data});
+        } catch (error) {
+            dispatch({ type: CONTACT_ERROR,
+            payload : error.response.msg });
+        };
+    };
+
 
     // Clear Contact
     const clearContacts = () => {
@@ -81,10 +109,7 @@ const ContactState = props => {
         dispatch({ type: CLEAR_CURRENT });
     }
 
-    // Update Contact
-    const updateContact = contact => {
-        dispatch({ type: UPDATE_CONTACT, payload: contact});
-    }
+
 
 
     //Filter Contacts
